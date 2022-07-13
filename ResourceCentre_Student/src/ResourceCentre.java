5fe94fd2f509af35e4ff13bc063235ea2ffa4be6
21009155 <21009155@LAPTOP-18GEIH4J>
2022-07-13 15:19:48 +0800

    committed by weihong

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -5,6 +5,7 @@ public class ResourceCentre {
 	public static void main(String[] args) {
 		//student 1 - bhavana
 		//student 2 - yeetengg
+		//student 3 - weihong
 		ArrayList<Camcorder> camcorderList = new ArrayList<Camcorder>();
 		ArrayList<Chromebook> chromebookList = new ArrayList<Chromebook>();
 
